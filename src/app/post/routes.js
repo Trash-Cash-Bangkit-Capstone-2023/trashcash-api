785f9683ed480e18bfc610dbcf5aa2c06a732c6d
pandemuliada <pandemuliada@gmail.com>
2023-05-17 22:26:34 +0800

    remove unused comment

--- src/app/post/routes.js
+++ src/app/post/routes.js
@@ -1,8 +1,3 @@
-// TODO : GET /posts -> Create index post route with search by tags
-// TODO : GET /posts/:id -> Create show post route
-// TODO : POST /posts/:id -> Create update post route
-// TODO : UPDATE /posts/:id -> Create delete post route
-
 const PostController = require("./controller");
 const postRoutes = async (fastify, options) => {
   fastify.get("/posts", PostController.getAll);
