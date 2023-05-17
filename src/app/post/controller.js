d847894add1147d6b9fe0e60a7a714217e099ecc
pandemuliada <pandemuliada@gmail.com>
2023-05-17 22:26:18 +0800

    :sparkles: update & delete post api

--- src/app/post/controller.js
+++ src/app/post/controller.js
@@ -16,6 +16,10 @@ const postSchema = {
   },
 };
 
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 5) + 1;
+};
+
 const PostController = {
   async getAll(req, reply) {
     // TODO : Create search by tags
@@ -67,7 +71,7 @@ const PostController = {
       req.body;
 
     // TODO : Add upload to storage bucket function here
-    const image_url = "";
+    const image_url = `https://picsum.photos/500?random=${getRandomNumber()}`;
 
     // Validation
     if (!title || !description || !category || !tags || !quantity || !user_id) {
@@ -104,8 +108,85 @@ const PostController = {
     });
   },
   async update(req, reply) {
-    const { id } = request.params;
-    reply.send({ message: "Update" });
+    const { id } = req.params;
+    const { title, description, category, tags, quantity, image } = req.body;
+
+    // TODO : Add upload to storage bucket function here
+    const image_url = `https://picsum.photos/500?random=${getRandomNumber()}`;
+
+    // Validation
+    if (!title || !description || !category || !tags || !quantity) {
+      reply.status(400).send({
+        errors: [{ message: "Bad request" }],
+      });
+    }
+
+    const postRef = await firestore().collection("posts").doc(id).get();
+    if (!postRef.exists) {
+      reply.status(404).send({
+        errors: [{ message: "Post not found" }],
+      });
+    }
+
+    const updatedPostRef = await firestore()
+      .collection("posts")
+      .doc(id)
+      .update({
+        title,
+        description,
+        category,
+        tags,
+        quantity,
+        image_url,
+      });
+
+    if (!updatedPostRef) {
+      reply.status(500).send({
+        errors: [{ message: "Internal server error" }],
+      });
+    }
+
+    const updatedPost = await firestore().collection("posts").doc(id).get();
+    const userRef = await firestore()
+      .collection("users")
+      .doc(updatedPost.data().user_id)
+      .get();
+    const user = userSerializer(userRef);
+
+    reply.code(200).send({
+      message: "Success",
+      data: {
+        post: { ...postSerializer(updatedPost), user },
+      },
+    });
+  },
+  async delete(req, reply) {
+    const { id } = req.params;
+
+    const postRef = await firestore().collection("posts").doc(id).get();
+    if (!postRef.exists) {
+      reply.status(404).send({
+        errors: [{ message: "Post not found" }],
+      });
+    }
+
+    const deletedPostRef = await firestore()
+      .collection("posts")
+      .doc(id)
+      .delete();
+
+    if (!deletedPostRef) {
+      reply.status(500).send({
+        errors: [{ message: "Internal server error" }],
+      });
+    }
+
+    reply.code(200).send({
+      message: "Success",
+      data: {
+        post: { ...postSerializer(postRef) },
+      },
+    });
   },
 };
 
--- src/app/post/routes.js
+++ src/app/post/routes.js
@@ -9,6 +9,7 @@ const postRoutes = async (fastify, options) => {
   fastify.get("/posts/:id", PostController.getById);
   fastify.post("/posts", PostController.create);
   fastify.put("/posts/:id", PostController.update);
+  fastify.delete("/posts/:id", PostController.delete);
 };
 
 module.exports = postRoutes;
