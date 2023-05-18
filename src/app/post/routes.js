103ea821d8cb3d177c0bec62065365c8a2331352
pandemuliada <pandemuliada@gmail.com>
2023-05-18 22:31:42 +0800

    :sparkles: add isAuthenticated middleware

--- src/app/post/routes.js
+++ src/app/post/routes.js
@@ -1,5 +1,8 @@
+const isAuthenticated = require("../../middlewares/isAuthenticated");
 const PostController = require("./controller");
+
 const postRoutes = async (fastify, options) => {
+  fastify.addHook("onRequest", isAuthenticated);
   fastify.get("/posts", PostController.getAll);
   fastify.get("/posts/:id", PostController.getById);
   fastify.post("/posts", PostController.create);
--- src/middlewares/isAuthenticated.js
+++ src/middlewares/isAuthenticated.js
@@ -1,16 +1,19 @@
 const { auth } = require("../services/firebase");
 
 const isAuthenticated = async (request, reply) => {
+  if (!request.headers.authorization) {
+    reply.status(401).send({ code: 401, message: "Unauthorized" });
+  }
+
+  const [, token] = request.headers.authorization.split("Bearer ");
   try {
-    const decodedToken = await auth().verifyIdToken(
-      request.headers.authorization,
-    );
+    const decodedToken = await auth().verifyIdToken(token);
     request.user = decodedToken;
 
     // Continue processing the request
   } catch (error) {
     console.error("Error verifying token:", error);
-    reply.status(401).send({ message: "Unauthorized" });
+    reply.status(401).send({ code: 401, message: "Unauthorized" });
   }
 };
 
