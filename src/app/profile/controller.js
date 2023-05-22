92c843a011c3444220b4d1688250eb9cca99cb68
pandemuliada <pandemuliada@gmail.com>
2023-05-22 12:15:19 +0800

    :sparkles: add update profile routes

--- src/app/profile/controller.js
+++ src/app/profile/controller.js
@@ -1,4 +1,4 @@
-const { firestore } = require("../../services/firebase");
+const { firestore, auth } = require("../../services/firebase");
 const { userSerializer } = require("../../serializers");
 
 const ProfileController = {
@@ -13,7 +13,46 @@ const ProfileController = {
     });
   },
   async update(req, reply) {
-    // TODO : Update details
+    const { user } = req;
+    const { name, province, address, phone, email } = req.body;
+
+    const userAuthRef = auth().updateUser(user.uid, {
+      displayName: name,
+      phoneNumber: phone,
+      email,
+      emailVerified: true,
+    });
+
+    const userRef = await firestore().collection("users").doc(user.uid).update({
+      name,
+      province,
+      address,
+      phone,
+      email,
+    });
+
+    if (!userRef || !userAuthRef) {
+      reply.status(500).send({
+        errors: [
+          {
+            message: "Internal server error",
+            error: { message: "Failed to update user" },
+          },
+        ],
+      });
+    }
+
+    const updatedUserRef = await firestore()
+      .collection("users")
+      .doc(user.uid)
+      .get();
+
+    reply.send({
+      message: "Success",
+      data: {
+        user: userSerializer(updatedUserRef),
+      },
+    });
     // TODO : Update image
   },
 };
--- src/app/profile/routes.js
+++ src/app/profile/routes.js
@@ -4,6 +4,7 @@ const ProfileController = require("./controller");
 const profileRoutes = async (fastify, options) => {
   fastify.addHook("onRequest", isAuthenticated);
   fastify.get("/profile", ProfileController.me);
+  fastify.put("/profile", ProfileController.update);
 };
 
 module.exports = profileRoutes;
