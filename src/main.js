781cf3cdc1a739ca2f9a41314ca046fcdea4ea3a
pandemuliada <pandemuliada@gmail.com>
2023-05-29 16:07:34 +0800

    :sparkles: add endpoint to predict image

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ require("dotenv").config();
 require("./services/firebase"); // Initialize firebase admin
 const fastify = require("fastify")({ logger: true });
 const cors = require("@fastify/cors");
+const tf = require("@tensorflow/tfjs-node");
+const sharp = require("sharp");
 
 // Routes
 const postRoutes = require("./app/post/routes");
@@ -12,6 +14,9 @@ const authRoutes = require("./app/auth/routes");
 fastify.register(require("@fastify/formbody"));
 fastify.register(require("@fastify/multipart"), {
   preservePath: true,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // 5MB
+  },
 });
 fastify.register(cors, {
   origin: ["*"], // TODO : Change into domain or ip later on
@@ -21,6 +26,50 @@ fastify.register(cors, {
 fastify.register(authRoutes, { prefix: "/v1" });
 fastify.register(postRoutes, { prefix: "/v1" });
 fastify.register(imageRoutes, { prefix: "/v1" });
+
+// Image classification
+fastify.post("/v1/image/predict", async (request, reply) => {
+  const model = await tf.loadLayersModel(
+    `file://src/model/converted/model.json`,
+  );
+  const file = await request.file();
+
+  try {
+    const imageBuffer = await file.toBuffer();
+    const preprocessedImage = await sharp(imageBuffer)
+      .resize(224, 224, {
+        fit: sharp.fit.cover,
+        position: sharp.strategy.entropy,
+      }) // Resize the image to the expected input size
+      .toFormat("jpeg") // Convert the image to JPEG
+      .toBuffer(); // Get the raw buffer from the processed image
+
+    // Decode the resized image Buffer into a tensor
+    const imageTensor = tf.node.decodeImage(preprocessedImage, 3);
+    // Normalize pixel values to [0, 1]
+    const normalizedTensor = imageTensor.div(255.0);
+    // Expand dimensions to match the expected input shape of [batchSize, height, width, channels]
+    const expandedTensor = normalizedTensor.expandDims();
+
+    const result = model.predict(expandedTensor);
+    const resultArray = Array.from(await result.data());
+    const classIndex = resultArray.indexOf(Math.max(...resultArray));
+    const classes = ["sellable", "unsellable"];
+    const classification = classes[classIndex];
+
+    reply.send({
+      message: "Image classification success",
+      data: {
+        classification,
+      },
+    });
+  } catch (err) {
+    reply.send(err);
+  }
+
+  reply.send({ message: "Oops, you're on the wrong path!" });
+});
+
 fastify.get("/", async (request, reply) => {
   reply.send({ message: "Hello World" });
 });
