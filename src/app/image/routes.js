5c4c4d31075b29efbac60b7471f8382a0705aa54
PremaNandika <nandika183@gmail.com>
2023-06-13 21:28:27 +0800

    :recycle: fix image thing and adding yamls

--- src/app/image/routes.js
+++ src/app/image/routes.js
@@ -1,6 +1,8 @@
 const ImageController = require("./controller");
+const isAuthenticated = require("../../middlewares/isAuthenticated");
 
 const imageRoutes = async (fastify, options) => {
+  fastify.addHook("onRequest", isAuthenticated);
   fastify.post("/image", ImageController.uploadImage);
 };
 
